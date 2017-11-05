a2c3c365e442505f3c50ce9f8126345614a5d9fa
Erick Chandra <erickchandra.1@gmail.com>
2017-11-05 22:26:11 +0700

    Added django-bower and admin-lte components settings.

--- blockchain_anaviz/settings.py
+++ blockchain_anaviz/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blockchain_anaviz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['static/bower_components'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
