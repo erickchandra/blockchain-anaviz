ee5b52a1d7e3317e9e3becc647b08fd0d114b179
Erick Chandra <erickchandra.1@gmail.com>
2017-11-12 20:00:26 +0800

    Added exchange stats.

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,10 @@ app_name = 'home'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^price-realtime', views.priceRealtime, name="priceRealtime"),
-    url(r'^price-historical', views.priceHistorical, name="priceHistorical")
+    url(r'^ticker-blockchain', views.tickerBlockchain, name="tickerBlockchain"),
+    url(r'^price-historical', views.priceHistorical, name="priceHistorical"),
+    url(r'^price-exchange', views.priceExchange, name="priceExchange"),
+    url(r'^ticker-bitfinex', views.tickerBitfinex, name="tickerBitfinex"),
+    url(r'^ticker-kraken', views.tickerKraken, name="tickerKraken"),
+    url(r'^depth-kraken', views.depthKraken, name="depthKraken"),
 ]
